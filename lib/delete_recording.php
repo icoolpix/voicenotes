6a3b8760c5e88172f97c2e05dc1b84ba7efddd6a
jgmuchiri <jgmuchiri@gmail.com>
2014-12-23 15:16:37 -0500

    Update

--- lib/delete_recording.php
+++ lib/delete_recording.php
@@ -12,28 +12,29 @@ require_once('config.php');
 if($_POST) {
 	$id = $_POST['note_id'];
 
-//get file name
+	//get file name
 	$db->where('id', $id);
 	$q = $db->get('notes');
 	foreach($q as $r) {
 		$name = $r['audio_file'];
 		$file = '../' . RECORD_DIR . $name;
 
+		//try to delete the file
 		if(file_exists($file)) {
 			if(unlink($file)) {
-				//delete from db
-				$db->where('id', $id);
-				if($db->delete('notes')) {
-					echo 'Recording deleted!';
-				} else {
-					echo 'Error deleting file';
-				}
-
-			} else {
+			}else {
 				echo "ERROR! Unable to delete file. Make sure you have permission";
 			}
 		} else {
-			echo 'ERROR! File not found!';
+			echo "ERROR! File not found!\n";
+		}
+
+		//delete from db (either way try it even if the file was not found)
+		$db->where('id', $id);
+		if($db->delete('notes')) {
+			echo "Recording deleted!";
+		} else {
+			echo "Error deleting file";
 		}
 	}
 }
